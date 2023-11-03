e9c0fbc83c508825c4449b0aa681b409b6dab8a9
Ekushey Computer <majedulislam117@gmail.com>
2023-11-03 23:36:25 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'dart:convert';
-import 'package:http/http.dart';
 import 'package:flutter/material.dart';
-
 
 void main() {
   runApp(const MyApp());
+}
+
+class WeatherData {
+  final String city;
+  final int temperature;
+  final String condition;
+  final int humidity;
+  final double windSpeed;
+
+  WeatherData(this.city, this.temperature, this.condition, this.humidity, this.windSpeed);
 }
 
 class MyApp extends StatelessWidget {
@@ -13,42 +21,52 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const MyHomePage(),
+      title: 'Weather App',
+      home: WeatherList(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key});
+class WeatherList extends StatefulWidget {
+  const WeatherList({Key? key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _WeatherListState createState() => _WeatherListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  List<Product> productList = [];
+class _WeatherListState extends State<WeatherList> {
+  List<WeatherData> weatherData = [];
 
   @override
   void initState() {
     super.initState();
-    getProducts();
-  }
-
-  void getProducts() async {
-
+    final jsonWeatherData = jsonDecode(jsonData) as List;
+    weatherData = jsonWeatherData.map((data) => WeatherData(
+      data['city'],
+      data['temperature'],
+      data['condition'],
+      data['humidity'],
+      data['windSpeed'].toDouble(),
+    )).toList();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Photo Gallery App'),
+        title: Text('Weather Info App'),
       ),
       body: ListView.builder(
-        itemCount: productList.length,
+        itemCount: weatherData.length,
         itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-
+          final data = weatherData[index];
+          return Card(
+            child: ListTile(
+            title: Text("City:${data.city}"),
+            subtitle: Text(
+              'Temperature: ${data.temperature}°C\nCondition: ${data.condition}\nHumidity: ${data.humidity}%\nWind Speed: ${data.windSpeed} km/h',
+            ),
+           ),
           );
         },
       ),
@@ -56,12 +74,42 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class Product {
-  final String AlbumId;
-  final String Id;
-  final String Title;
-  final String Url;
-  final String ThumbnailUrl;
-
-  Product(this.AlbumId, this.Id, this.Title, this.Url, this.ThumbnailUrl);
-}
+const String jsonData = '''
+[
+  {
+    "city": "New York",
+    "temperature": 20,
+    "condition": "Clear",
+    "humidity": 60,
+    "windSpeed": 5.5
+  },
+  {
+    "city": "Los Angeles",
+    "temperature": 25,
+    "condition": "Sunny",
+    "humidity": 50,
+    "windSpeed": 6.8
+  },
+  {
+    "city": "London",
+    "temperature": 15,
+    "condition": "Partly Cloudy",
+    "humidity": 70,
+    "windSpeed": 4.2
+  },
+  {
+    "city": "Tokyo",
+    "temperature": 28,
+    "condition": "Rainy",
+    "humidity": 75,
+    "windSpeed": 8.0
+  },
+  {
+    "city": "Sydney",
+    "temperature": 22,
+    "condition": "Cloudy",
+    "humidity": 55,
+    "windSpeed": 7.3
+  }
+]
+''';
